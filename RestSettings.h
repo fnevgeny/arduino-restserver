c7a33e3b44a7df01522ed1bd06d40ef2fa36249a
Evgeny <evgeny@si-o-net.com>
2020-08-14 18:13:22 +0300

    Fix trailings after #endif's

--- RestSettings.h
+++ RestSettings.h
@@ -15,11 +15,11 @@
 
 #ifndef HTTP_COMMON_HEADER
 #define HTTP_COMMON_HEADER "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: POST, GET, PUT, OPTIONS\r\nContent-Type: application/json\r\nConnection: close\r\n"
-#endif HTTP_COMMON_HEADER
+#endif /* HTTP_COMMON_HEADER */
 
 #ifndef OUTPUT_BUFFER_SIZE
 #define OUTPUT_BUFFER_SIZE 250
-#endif OUTPUT_BUFFER_SIZE
+#endif /* OUTPUT_BUFFER_SIZE */
 
 #define JSON_START() addToBuffer("{ ");
 #define JSON_CLOSE() addToBuffer(" }");
